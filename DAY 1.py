c2cf3f7134b3d57842469b65eba31b6812848f92
jayanthsenthil29 <jayanthsenthil29@gmail.com>
2026-05-09 11:44:10 +0530

    Add files via upload

---
+++ DAY 1.py
@@ -0,0 +1,197 @@
+a=10
+print(a)
+
+
+#arithmetic operator
+
+a=14
+print(a)
+b=7
+print(b)
+print("addition",a+b)
+print("subtraction",a-b)
+print("multiplication",a*b)
+print("floor division",a/b)
+print("integer division",a//b)
+print("remainder",a%b)
+print("exponentiation",a**b)
+
+#assigment operator
+x=5
+x+=5
+print(x)
+x-=5
+print(x)
+
+
+x*=5
+print(x)
+x=6
+x/=6
+print(x)
+x%=6
+print(x)
+x//=7
+print(x)
+x=6
+x**=6
+print(x)
+
+#comparisional operator
+age1=10
+age2=30
+print(age1==age2)
+print(age1>age2)
+print(age1<age2)
+print(age1<=age2)
+print(age1>=age2)
+
+
+#special operators
+
+#identity operator
+
+x=["apple","coconut"]
+y=["apple","coconut"]
+
+print(x is y)
+
+print(x is not y)
+
+
+x=y
+
+
+print(x is y )
+
+
+
+#membership operator
+   
+t=["a","b","c"]
+
+if "b" not in t:
+    print("present")
+
+else:
+    print("not present")
+        
+
+#bitwise operators
+a=7
+b=8
+print(a&b)
+print(a|b)
+print(a^b)
+print(~a)
+print(a<<2)    
+print(a>>2)
+
+#logical operators
+
+
+level=20
+health=7
+
+if(level>25) and (health>5):
+    print("next level")
+    
+else:
+        print("failed")
+
+
+level=20
+health=7
+
+if(level>25) or (health>5):
+    print("next level")
+else:
+        print("failed")
+
+        a=true
+        print(not(a))
+
+        a=10
+        if not (a>15):
+            print("greater")
+        else:
+            print("less")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
