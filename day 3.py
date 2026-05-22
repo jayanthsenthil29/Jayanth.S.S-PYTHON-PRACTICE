b0620c0c4cc8e69fff29a0aaa90c4c1e6dcfcfb8
jayanthsenthil29 <jayanthsenthil29@gmail.com>
2026-05-22 11:51:47 +0530

    Add files via upload

---
+++ day 3.py
@@ -0,0 +1,123 @@
+'''#1 sleep question
+sleep_hours=int(input("enter the sleep hours"))
+mood=int(input("enter the mood"))              
+if (sleep_hours>=7)and (mood>=7):
+          print("productive human")
+elif (sleep_hours<5)and (mood>7):
+    print("zombie mode")
+else:
+    print("need coffee")'''
+    
+
+'''#2 netflix password
+password=int(input("enter the password"))
+alphanumericals=str(input("enter the alphanumericals")
+if (password>8) and (alphanumericals):
+             print("access granted")
+else:
+    print("weak password")'''
+
+    
+'''#3 free fire unlock system
+level=int(input("enter your level"))
+diamonds=int(input("enter your level"))
+if (level>20) and (diamonds>100):
+             print("elite pass unlocked")
+else:
+    print("grind more")'''
+
+    
+'''#4 pizza billing system
+print("menu:veg pizza=120,chicken pizza=180,extra cheese=+40")
+pizza=input("select pizza")
+cheese=input("cheese option")
+vegpizza=120
+chickenpizza=180
+extracheese=+40
+if pizza=="veg pizza" and cheese=="extra cheese":
+    print("160")
+elif pizza=="chicken pizza" and cheese=="extra cheese":
+    print("220")
+elif pizza=="veg pizza" and cheese=="no cheese":
+    print("120")
+elif pizza=="chicken pizza" and cheese=="no cheese":
+    print("180")'''
+
+'''#5 lucky number
+number=int(input("enter the number"))
+if number%3<1 and number%5<1:
+    print("fizzbuzz")
+elif number%5<1:
+    print("buzz")
+elif number%3<1:
+    print("fizz")
+else:
+    print("normal human")'''
+
+'''#6
+number=int(input("enter the number"))
+if number%3<1 and number%2<1:
+    print("code master")
+elif number%2<1:
+    print("even energy")
+elif number%3<1:
+    print("odd wizard")
+else:
+    print("NPC Number")'''
+
+'''#7 money
+money=int(input("enter the money"))
+if money>40:
+            print("return balance")
+elif money==40:
+              print("enjoy juice")
+else:
+     print("not enough money")'''
+
+'''#8
+world=input("enter a world")
+if world=="if" or world=="else" or world=="while" or world=="for":
+    print("me")'''
+#9
+lastseen=int(input("last seen hours"))
+reply=input("yes/no")
+if lastseen<1 and reply=="no":
+             print("ghosting")
+elif lastseen==0 and reply=="yes":
+    print("available")
+elif lastseen>24:
+    print("busy")
+     
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
