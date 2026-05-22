bb03bffe7d6ed290bf0b50c5c547f6c4855d142c
jayanthsenthil29 <jayanthsenthil29@gmail.com>
2026-05-22 13:36:39 +0530

    Add files via upload

---
+++ list function.py
@@ -0,0 +1,92 @@
+'''#list function
+
+#append
+india=["TN","KERLA","SIKKIM"]
+india.append("DELHI")
+print(india)
+
+#extend
+whole_num = [0,1,2,3,4]
+whole_num.extend([5,6,7,8])
+print(whole_num)'''
+
+'''#insert
+cards=["ace","queen","jack"]
+cards.insert(1,"king")
+print(cards)
+
+#remove
+india=["tn","kerla","delhi"]
+india.remove("kerla")
+print(india)
+
+#pop
+num = [10,20,30,40]
+item = num.pop()
+print(item)
+print(num)
+
+#clear
+p=["kanishk","janaa","wafid"]
+p.clear()
+print(p)
+
+#index
+allies = ['USA', 'UK', 'USSR', 'FRANCE']
+print(allies.index("USSR"))
+
+#count
+num = [5,55,6,6,6,1,2,2,2,3,]
+print(num.count(6))
+
+#sort
+num=[2,3,5,6,7,5,5]
+num.sort
+print(num)'''
+  
+#reverse
+india=["tn","delhi"]
+india.reverse()
+print(india)
+
+
+#copy
+axis = ["germany","italy","imperial japan"]
+new = axis.copy()
+print(new)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+       
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
