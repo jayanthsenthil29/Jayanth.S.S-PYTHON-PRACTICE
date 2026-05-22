840c8e1b6bf84bdcccb76f5864387ace7032ff72
jayanthsenthil29 <jayanthsenthil29@gmail.com>
2026-05-22 13:03:37 +0530

    Add files via upload

--- day 6 data structure.py
+++ day 6 data structure.py
@@ -1,2 +1,64 @@
-Python 3.14.4 (tags/v3.14.4:23116f9, Apr  7 2026, 14:10:54) [MSC v.1944 64 bit (AMD64)] on win32
-Enter "help" below or click "Help" above for more information.
+'''a=10,20,30,40,50,67,45,7,68
+print(type(a))
+
+
+b=30,
+print(type(b))
+
+
+#method 1
+a=[]#single value
+a=[10,20,30,40,50,67,45,7,68]#multiple values
+print(type(a))
+
+#method 2
+a=list((10,20,30,6))
+print(type(a))
+
+#tuple
+s=()
+a=(10,20,30,40,50,67,45,7,68)
+print(type(a))
+
+b=tuple([4,3,5,6])
+print(type(b))
+
+#set
+s={10,8,9,4,6}
+print(type(s))
+
+s=set()
+print(type(s))
+
+#dictionary
+d={1:10,2:8,4:4,3:9}
+
+'''
+#list are mutable/changeable
+data=["Janani",1234 , 1 ,"PHONE", 50000, 2245,1234 ]
+print(data)
+data.append(2000)
+print(data)
+
+#strings are immutable
+s="hello"
+print(s)
+print(s.capitalize())
+print(s)
+'
+data=("Janani",1234 , 1 ,"PHONE", 50000, 2245 ,1234)
+print(data)
+print(type(data))
+
+
+data={"Janani",1234 , 1 ,"PHONE", 50000, 2245 }
+print(data)
+print(type(data))
+
+
+data={"name":"Janani","pwd":1234 , "quantity":1 ,"product":"PHONE", "price":50000, "pwd":1234 }
+print(data)
+print(type(data))
+'''
+
+
