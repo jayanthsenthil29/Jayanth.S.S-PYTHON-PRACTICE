52be110f6fe2378656b6883df93be960df33f91f
jayanthsenthil29 <jayanthsenthil29@gmail.com>
2026-05-22 11:52:22 +0530

    Add files via upload

---
+++ DAY 2 PROBLEMS.py
@@ -0,0 +1,67 @@
+#4 question
+a=12345
+x=3
+x*=3
+print(x)
+
+
+#8 question
+r=2
+h=4
+print("multiplication",3.14*r*r*h)
+#wrong code
+l=1
+b=2
+h=3
+print("multiplication",l*b*h)
+
+
+#6 question
+
+w=50
+h=1.50
+print(w/h**2)
+
+
+#1 question
+
+'''x=98765
+last_digit=x%10
+print(last_digit)
+
+#3 question
+
+x=98765
+last_two_digit=x%100
+print(last_two_digit)
+
+
+#2  question
+
+x=98768
+y=10
+print(x//y)'''
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
