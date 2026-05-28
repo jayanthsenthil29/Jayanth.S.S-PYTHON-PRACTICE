e77e59fd5891d70133e714657387bd5cd4d67f9e
jayanthsenthil29 <jayanthsenthil29@gmail.com>
2026-05-28 12:30:48 +0530

    Add files via upload

---
+++ PROBLEMS DAY 9.py
@@ -0,0 +1,65 @@
+"""#1.Create a list with 5 numbers and print it.
+a=(10,20,30,40,50)
+print(a)"""
+
+"""#2.Add a new element to a list using append().
+
+a=[10,20,30,40,50]
+a.append(60)
+print(a)"""
+
+"""#3.Insert an element at index 2.
+a=[10,20,30,40,50]
+a.insert(2,100)
+print(a)"""
+
+"""#4.Remove an element using remove().
+a=[10,20,30,40,50]
+a.remove(30)
+print(a)"""
+
+"""#5.move last element using pop().
+a=[10,20,30,40,50]
+a.pop()
+print(a)"""
+
+"""#6.Find the length of a list
+a=[10,20,30,40,50]
+print(len(a))"""
+
+"""#7.Find maximum and minimum number.
+
+a=[10,20,30,40,50]
+print("maximum number",max(a))
+print("minimum number",min(a))
+
+#8.Find sum of all elements.
+a=[10,20,30,40,50]
+print(sum(a))"""
+
+#9.Count how many times a number appears
+a=["enter the value"]
+print(a.count(a))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
