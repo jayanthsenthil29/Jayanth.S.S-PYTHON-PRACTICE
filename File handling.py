486ba74e54a6c9a8f1a433af4fd9b624ee69009c
jayanthsenthil29 <jayanthsenthil29@gmail.com>
2026-06-10 18:21:03 +0530

    Add files via upload

---
+++ File handling.py
@@ -0,0 +1,25 @@
+"""#a=open("C:\\Users\\Jayanthsenthil\\OneDrive\\Desktop\\khabib.txt","x")
+print("done")
+
+
+with open("C:\\Users\\Jayanthsenthil\\OneDrive\\Desktop\\khabib.txt","w") as a:
+    a.write("janaa vs dharshan")
+    a.close
+print("done")"""
+
+
+"""with open("C:\\Users\\Jayanthsenthil\\OneDrive\\Desktop\\khabib.txt","a") as a:
+    a.write("janaa won")
+    a.close
+
+
+with open("C:\\Users\\Jayanthsenthil\\OneDrive\\Desktop\\khabib.txt","r") as a:
+    print(a.read())
+    a.close()"""
+
+import os
+
+os.remove("C:\\Users\\Jayanthsenthil\\OneDrive\\Desktop\\jayanth.txt")
+
+
+
