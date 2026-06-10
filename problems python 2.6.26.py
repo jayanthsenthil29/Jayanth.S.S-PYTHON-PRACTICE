507eaeecd2149d68c4e4feec4f5f530af7ca14fa
jayanthsenthil29 <jayanthsenthil29@gmail.com>
2026-06-10 18:20:28 +0530

    Add files via upload

---
+++ problems python 2.6.26.py
@@ -0,0 +1,96 @@
+"""#1.Find the square root of a number
+s=int(input("enter the number"))
+s=s*s
+print(s)"""
+
+"""#2.Calculate the factorial of 7
+import math
+print(math.factorial(7))"""
+
+
+"""#3.Find the value of π
+import math
+print(math.pi)"""
+
+"""#4.Convert 180 degrees to radians
+import math
+print(math.radians(180))"""
+
+"""#5.Convert π radians to degrees
+import math
+print(math.degrees(180))"""
+
+"""#6.Find the GCD of 48 and 18
+import math
+print(math.gcd(48, 18))"""
+
+"""#7.Find the LCM of 12 and 15
+import math
+print(math.lcm(12,15))"""
+
+"""#8.Find the ceiling and floor values of 15.7
+import math
+print(math.ceil(15.7))
+
+import math
+print(math.floor(15.7))"""
+
+"""#9.Generate a random number between 1 and 100
+import random
+print(random.randint(1,100))"""
+
+"""#10.Pick a random fruit from a list
+import random
+fruits = ['apple', 'dragon fruit', 'cherry', 'date']
+print(random.choice(fruits))"""
+
+"""#11.Toss a coin
+import random
+print(random.choice(['Heads', 'Tails']))"""
+
+"""#12.Roll a dice 
+import random
+print(random.randint(1,6))"""
+
+"""#13.Shuffle a deck of cards.
+import random
+suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades']
+ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace']
+deck = [f'{rank} of {suit}' for suit in suits for rank in ranks]
+random.shuffle(deck) 
+print(deck)"""
+
+"""#14.Generate 10 random numbers and find the maximum.
+import random as r
+random_numbers = [r.randint(1, 90) for _ in range(10)]
+print(random_numbers)
+print(max(random_numbers))"""
+
+"""#15.Pick 4 random students from a class list.
+import random as s
+print(s.choice(["janaa","jayanth","kanishk","wafid"]))"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
