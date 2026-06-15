a08576377a33f5fbc5e6b5766cbd40ff49f60db8
jayanthsenthil29 <jayanthsenthil29@gmail.com>
2026-06-15 13:36:48 +0530

    Add files via upload

---
+++ test 15.6.26.py
@@ -0,0 +1,30 @@
+"""#1.Remove_duplicates:
+
+duplicate=[1,2,2,3]
+print(set (duplicate))"""
+
+"""#2.finding odd and even numbers
+
+list=[10,20,30,45,55]
+odd=0
+even=0
+
+for num in list:
+ if num%2==0:
+    even=even +1
+ else:
+    odd=odd +1
+print("even number count:",even)
+print("odd number count:",odd)"""
+
+"""#3.ascending order
+
+list=[0,1,12,5,56]
+list.sort()
+print(list)"""
+
+
+
+
+
+
